2c0dabf77d2d5604ca2973ad274cdb9d342646db
Dmytro Levchuk <dmytro_levchuk@epam.com>
2019-12-04 21:38:12 +0200

    atata

---
+++ client/build/precache-manifest.14c5d90d9808f942b5aa5a56f17ee5ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5968d1b5f8876aba3e09e5512e8e1d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72a144c470b887710687",
+    "url": "/static/css/main.81bb946c.chunk.css"
+  },
+  {
+    "revision": "84f5b4075bb91dfcabaa",
+    "url": "/static/js/2.5a71b968.chunk.js"
+  },
+  {
+    "revision": "72a144c470b887710687",
+    "url": "/static/js/main.6c78077e.chunk.js"
+  },
+  {
+    "revision": "1337678ae3301a2c8a7b",
+    "url": "/static/js/runtime-main.8eac158a.js"
+  }
+]);
